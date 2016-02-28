95112589a440cf6fe964c7de09e754c9ab7cfd53
anonymoussc <50c5ac69@opayq.com>
2016-02-28 13:13:12 +0700

    Add footer

---
+++ resources/angular-material/layouts/minimalist/footer/footer.controller.js
@@ -0,0 +1,31 @@
+/**
+ * footer.controller
+ * Created by anonymous on 28/02/16 11:20.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('quetzalcoatl')
+        .controller('FooterController', FooterController);
+
+    FooterController.$inject = [];
+
+    /* @ngInject */
+    function FooterController() {
+
+        var vm   = this;
+
+
+        activate();
+
+        ////////////////
+
+        function activate() {
+            //
+        }
+    }
+
+})();
+
