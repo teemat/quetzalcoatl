e5411fdd0d6898507207eaf63e1351be5f232809
anonymoussc <50c5ac69@opayq.com>
2016-02-28 08:47:26 +0700

    New quetzalcoatl.module

---
+++ resources/angular-material/quetzalcoatl.module.js
@@ -0,0 +1,18 @@
+/**
+ * quetzalcoatl.module.js
+ * Created by anonymous on 28/02/16 8:44.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('quetzalcoatl', [
+
+            /* Shared */
+            'appFoundation',
+            'widgets'
+
+        ]);
+
+})();
